f1164a30d1ec4b0bf91690e00f1e9f9ed669834f
Pratik Ambani <pratikaambani@gmail.com>
2018-06-16 15:06:15 +0530

    inline html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+//   template: `
+//   <p> This is inline html</p>
+// `,
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
