35da92c33a3e129431a028d9b62c50a980e4d51a
Pratik Ambani <pratikaambani@gmail.com>
2018-06-17 18:31:11 +0530

    routing: nevigating between components

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, Route } from '@angular/router';
 
 
 @Component({
@@ -9,11 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AboutComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private router: Router) {
     this.route.params.subscribe(res => console.log('Bingo!! We were successfully able to retrieve value from query parameter: ' +res.id));
   }
-
+  
   ngOnInit() {
+  }
+
+  sendMeHome() {
+    this.router.navigate(['']);
   }
 
 }
