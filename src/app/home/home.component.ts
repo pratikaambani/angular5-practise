65e02d2cc7ed61911ecb83860f0dfcf636433799
Pratik Ambani <pratikaambani@gmail.com>
2018-06-17 17:19:34 +0530

    removing data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,11 +19,19 @@ animations: [
       query(':enter', style({opacity: 0}), {optional: true}),
       query(':enter', stagger('300ms', [
         animate('.6s ease-in', keyframes([
-          style({opacity: 0, transform: 'translateY(-75%', offset: 0}),
-          style({opacity: 0.5, transform: 'translateY(35px', offset: 0.3}),
-          style({opacity: 1, transform: 'translateY(0', offset: 1}),
+          style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+          style({opacity: 0.5, transform: 'translateY(35px)', offset: 0.3}),
+          style({opacity: 1, transform: 'translateY(0)', offset: 1}),
         ]) )
-      ]),{optional: true})
+      ]),{optional: true}),
+
+      query(':leave', stagger('300ms', [
+        animate('.6s ease-in', keyframes([
+          style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+          style({opacity: 0.5, transform: 'translateY(35px)', offset: 0.3}),
+          style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+        ]) )
+      ]),{optional: true}),
     ])
   ])
 ]
@@ -46,6 +54,11 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i) {
+    this.goals.splice(i, 1);
+
   }
 
 }
